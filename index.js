f0f0217b6a86a0bda73341edb8c89e934aecfa62
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-05-13 21:32:40 +0530

    Refactor calculator logic

--- index.js
+++ index.js
@@ -2,74 +2,66 @@ const controls = document.querySelectorAll("button");
 const input = document.getElementById("result");
 const clearButton = document.getElementById("clearButton");
 const deleteButton = document.getElementById("deleteButton");
-expression = "";
+let expression = "";
 controls.forEach((control) =>
   control.addEventListener("click", (event) => {
-    if (event.target.id != "deleteButton" && event.target.id != "clearButton") {
-      if (event.target.id == "=" && expression.length == 0) {
-        input.placeholder = "Please enter a value";
-        return;
+    if (
+      event.target.id !== "deleteButton" &&
+      event.target.id !== "clearButton" &&
+      event.target.id !== "="
+    ) {
+      if (
+        (event.target.id === "+" ||
+          event.target.id === "-" ||
+          event.target.id === "*" ||
+          event.target.id === "/") &&
+        (expression.includes("+") ||
+          expression.includes("-") ||
+          expression.includes("*") ||
+          expression.includes("/"))
+      ) {
+        operate();
       }
       expression += event.target.id;
       input.value = expression;
-      if (event.target.id == "=") {
-        operate();
-      }
-      if (
-        (expression.includes("+") &&
-          expression.indexOf("+") != expression.lastIndexOf("+")) ||
-        expression[expression.length - 1] == "-" ||
-        expression[expression.length - 1] == "/" ||
-        expression[expression.length - 1] == "*"
-      ) {
-        operate();
-        expression += event.target.id;
-        input.value += event.target.id;
-      }
+    } else if (event.target.id === "=") {
+      operate();
     }
   })
 );
 
 function operate() {
+  let result;
   if (expression.includes("+")) {
-    expression = splitExpression(expression.slice(0, -1), "+");
-    input.value = expression;
+    result = splitExpression(expression, "+");
   } else if (expression.includes("-")) {
-    expression = splitExpression(expression.slice(0, -1), "-");
-    input.value = expression;
+    result = splitExpression(expression, "-");
   } else if (expression.includes("*")) {
-    expression = splitExpression(expression.slice(0, -1), "*");
-    input.value = expression;
+    result = splitExpression(expression, "*");
   } else if (expression.includes("/")) {
-    expression = splitExpression(expression.slice(0, -1), "/");
-    input.value = expression;
+    result = splitExpression(expression, "/");
+  }
+
+  if (!isNaN(result)) {
+    expression = "∞";
+    input.value = "∞";
   }
 }
 
 function splitExpression(exp, symbol) {
-  values = [];
-  values.push(exp.slice(0, exp.indexOf(symbol)));
-  values.push(exp.slice(exp.indexOf(symbol) + 1, exp.length));
-  if (symbol == "+") {
-    if (expression.includes(".")) {
-      return (Number(values[0]) + Number(values[1])).toFixed(2);
-    }
-    return Number(values[0]) + Number(values[1]);
-  } else if (symbol == "-") {
-    if (expression.includes(".")) {
-      return (Number(values[0]) - Number(values[1])).toFixed(2);
-    }
-    return Number(values[0]) - Number(values[1]);
-  } else if (symbol == "*") {
-    if (expression.includes(".")) {
-      return (Number(values[0]) * Number(values[1])).toFixed(2);
-    }
-    return Number(values[0]) * Number(values[1]);
-  } else if (symbol == "/") {
-    if (expression.includes(".")) {
-      return (Number(values[0]) / Number(values[1])).toFixed(2);
-    }
-    return Number(values[0]) / Number(values[1]);
+  let values = exp.split(symbol);
+  let num1 = Number(values[0]);
+  let num2 = Number(values[1]);
+
+  switch (symbol) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
   }
 }
 
